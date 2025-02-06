13cc8f6b3c8e65c2b0d3e9d0e20d6ab6832ddefe
hansojoong <13tkdkf@gmail.com>
2025-02-06 17:19:43 +0900

    한소중

---
+++ src/main/java/org/zerock/projects/service/machines/IProcessService.java
@@ -0,0 +1,9 @@
+package org.zerock.projects.service.machines;
+
+import org.zerock.projects.domain.ProductionOrder;
+import org.zerock.projects.domain.machines.Process;
+
+public interface IProcessService {
+    void saveProcess(Process process);
+    void moveToNextProcess(ProductionOrder order);
+}
